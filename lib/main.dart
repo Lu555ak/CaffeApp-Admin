273f416824c651a8184c58aaa07898f4cc2716e6
Lu555ak <grgecicluka@gmail.com>
2023-04-22 11:26:35 +0200

    Hierarchy structure optimisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 //import 'get_started_page.dart';
-import 'app_page.dart';
-import 'pages/home_page_desktop.dart';
-import 'pages/home_page_mobile.dart';
-import 'pages/home_page_tablet.dart';
+
+import 'desktop/home_page_desktop.dart';
+import 'mobile/home_page_mobile.dart';
+import 'tablet/home_page_tablet.dart';
 import 'custom/background.dart';
 
 void main() {
@@ -29,5 +29,29 @@ class MyApp extends StatelessWidget {
         ),
       ]),
     );
+  }
+}
+
+class CaffeApp extends StatelessWidget {
+  final Widget mobileLayout;
+  final Widget tabletLayout;
+  final Widget desktopLayout;
+
+  CaffeApp(
+      {required this.mobileLayout,
+      required this.tabletLayout,
+      required this.desktopLayout});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth <= 400) {
+        return mobileLayout;
+      } else if (constraints.maxWidth > 400 && constraints.maxWidth <= 1100) {
+        return tabletLayout;
+      } else {
+        return desktopLayout;
+      }
+    });
   }
 }
