6b9a4a8fb2d8b0fc0a3db45d35e8dd40be677582
Lu555ak <grgecicluka@gmail.com>
2023-05-04 19:38:47 +0200

    Fixed horizontal scroll

--- lib/mobile/home_page_mobile.dart
+++ lib/mobile/home_page_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:caffe_app/utility/constants.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class HomePageMobile extends StatefulWidget {
@@ -9,6 +10,8 @@ class HomePageMobile extends StatefulWidget {
 }
 
 class _HomePageMobileState extends State<HomePageMobile> {
+  final ScrollController scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -22,123 +25,133 @@ class _HomePageMobileState extends State<HomePageMobile> {
               borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(25.0),
                   bottomRight: Radius.circular(25.0))),
-          child: Text(
+          child: const Text(
             "ACTIVE ORDERS: ",
-            style: const TextStyle(
+            style: TextStyle(
                 color: secondaryColor,
                 fontWeight: FontWeight.w600,
                 fontSize: 15),
           ),
         ),
-        Container(
+        SizedBox(
           height: 200,
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: secondaryColor,
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                    ),
-                    child: Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: Container(
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 150,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Icon(
-                                      Icons.table_restaurant_rounded,
-                                      color: subColor2,
-                                      size: 35,
-                                    ),
-                                    Text(
-                                      "23",
-                                      style: TextStyle(fontSize: 25),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const Divider(
-                                color: primaryColor,
-                                indent: 20,
-                                endIndent: 20,
-                              ),
-                              Container(
-                                width: 150,
-                                child: ElevatedButton(
-                                  onPressed: () {},
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all(primaryColor),
-                                  ),
-                                  child: const Text('VIEW MORE'),
-                                ),
-                              ),
-                              const Divider(
-                                color: primaryColor,
-                                indent: 20,
-                                endIndent: 20,
-                              ),
-                              Row(
-                                children: [
-                                  const Divider(
-                                    color: primaryColor,
-                                    indent: 5,
-                                    endIndent: 5,
-                                  ),
+          width: double.infinity,
+          child: ScrollConfiguration(
+            behavior: CustomScrollBehavior(),
+            child: ListView.builder(
+                controller: scrollController,
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
                                   Icon(
-                                    Icons.access_time_filled,
+                                    Icons.table_restaurant_rounded,
                                     color: subColor2,
-                                    size: 33.5,
+                                    size: 35,
                                   ),
-                                  Text(" 2s"),
-                                  const Divider(
-                                    color: primaryColor,
-                                    indent: 5,
-                                    endIndent: 5,
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 40),
-                                    child: ElevatedButton(
-                                      onPressed: () {},
-                                      style: ButtonStyle(
-                                        shape: MaterialStateProperty.all(
-                                            const CircleBorder()),
-                                        padding: MaterialStateProperty.all(
-                                            const EdgeInsets.all(10)),
-                                        backgroundColor:
-                                            MaterialStateProperty.all(
-                                                subColor2),
-                                        overlayColor: MaterialStateProperty
-                                            .resolveWith<Color?>((states) {
-                                          if (states.contains(
-                                              MaterialState.pressed)) {
-                                            return subColor;
-                                          }
-                                          return null;
-                                        }),
-                                      ),
-                                      child: const Icon(Icons.check),
-                                    ),
+                                  Text(
+                                    "23",
+                                    style: TextStyle(fontSize: 25),
                                   ),
                                 ],
                               ),
-                            ],
-                          ),
-                        )),
-                  ),
-                );
-              }),
+                            ),
+                            const Divider(
+                              color: primaryColor,
+                              indent: 20,
+                              endIndent: 20,
+                            ),
+                            SizedBox(
+                              width: 150,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(primaryColor),
+                                ),
+                                child: const Text('VIEW MORE'),
+                              ),
+                            ),
+                            const Divider(
+                              color: primaryColor,
+                              indent: 20,
+                              endIndent: 20,
+                            ),
+                            Row(
+                              children: [
+                                const Divider(
+                                  color: primaryColor,
+                                  indent: 5,
+                                  endIndent: 5,
+                                ),
+                                const Icon(
+                                  Icons.access_time_filled,
+                                  color: subColor2,
+                                  size: 33.5,
+                                ),
+                                const Text(" 2s"),
+                                const Divider(
+                                  color: primaryColor,
+                                  indent: 5,
+                                  endIndent: 5,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 40),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ButtonStyle(
+                                      shape: MaterialStateProperty.all(
+                                          const CircleBorder()),
+                                      padding: MaterialStateProperty.all(
+                                          const EdgeInsets.all(10)),
+                                      backgroundColor:
+                                          MaterialStateProperty.all(subColor2),
+                                      overlayColor: MaterialStateProperty
+                                          .resolveWith<Color?>((states) {
+                                        if (states
+                                            .contains(MaterialState.pressed)) {
+                                          return subColor;
+                                        }
+                                        return null;
+                                      }),
+                                    ),
+                                    child: const Icon(Icons.check),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ),
         )
       ],
     );
   }
+}
+
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
