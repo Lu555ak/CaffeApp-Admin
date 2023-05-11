be6417d6423bb4db3b40fc733a3379a2bad36ca1
Lu555ak <grgecicluka@gmail.com>
2023-05-11 22:56:45 +0200

    Quiz model - question types

--- lib/models/quiz_model.dart
+++ lib/models/quiz_model.dart
@@ -68,11 +68,70 @@ class QuizQuestion {
 
   QuizQuestion(this._question);
 
-  String getQuestion() {
-    return _question;
+  String getQuestion() => _question;
+  void setQuestion(String question) => _question = question;
+}
+
+// True/False
+class QuizQuestionTrueFalse implements QuizQuestion {
+  @override
+  String _question;
+  bool _answer;
+
+  QuizQuestionTrueFalse(this._question, this._answer);
+
+  @override
+  String getQuestion() => _question;
+  @override
+  void setQuestion(String question) => _question = question;
+  bool getAnswer() => _answer;
+  void setAnswer(bool answer) => _answer = answer;
+}
+
+// Multiple Answers
+class QuizQuestionMultiple implements QuizQuestion {
+  @override
+  String _question;
+  final Map<String, bool> _answers = {};
+
+  QuizQuestionMultiple(this._question);
+
+  @override
+  String getQuestion() => _question;
+  @override
+  void setQuestion(String question) => _question = question;
+
+  void addAnswer(String statement, bool isCorrect) {
+    _answers[statement] = isCorrect;
   }
 
-  void setQuestion(String question) {
-    _question = question;
+  void removeAnswer(String statement) {
+    _answers.remove(statement);
+  }
+}
+
+// Fill-in
+class QuizQuestionFillIn implements QuizQuestion {
+  @override
+  String _question;
+  final List<String> _answers = List.empty(growable: true);
+
+  QuizQuestionFillIn(this._question);
+
+  @override
+  String getQuestion() => _question;
+  @override
+  void setQuestion(String question) => _question = question;
+
+  void addAnswer(String answer) {
+    _answers.add(answer);
+  }
+
+  String getAnswerAt(int index) {
+    return _answers[index];
+  }
+
+  void removeAnswerAt(int index) {
+    _answers.removeAt(index);
   }
 }
